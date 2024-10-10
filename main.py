ad3edc7c7b9d0230e0dcdcc39e3301ea92d17c9c
PenaSMiguel92 <miguel.pena11.19.92@gmail.com>
2024-10-09 20:10:43 -0700

    added iteration through files

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import internetarchive
+import time
 from tqdm import tqdm
 import threading
 import zipfile
 import os
 
+games_to_download = ['Castlevania', 'Conker\'s Bad Fur Day', 'Donkey Kong 64', \
+                     'F-ZERO X', 'GoldenEye 007', 'Harvest Moon 64', 'Legend of Zelda, The - Majora\'s Mask', \
+                     'Legend of Zelda, The - Ocarina of Time', 'Nightmare Creatures', 'Pokemon Stadium' \
+                     'Pokemon Stadium 2', 'Rampage - World Tour', 'San Francisco Rush - Extreme Racing', \
+                     'Star Fox 64', 'Super Mario 64', 'Super Smash Bros.']
 
 
 def main():
-    
     # test = internetarchive.search('https://archive.org/details/nintendo-64-romset-usa')
-    print(test)
+    search = internetarchive.search_items('identifier:nintendo-64-romset-usa')
+    for result in search:
+        itemid = result['identifier']
+        item = internetarchive.get_item(itemid)
+        files = item.get_files()
+        for file in tqdm(files):
+            if file.name[-4:] != '.zip':
+                continue 
+            bracket_index = file.name.find('(')
+            game_name = file.name[:bracket_index-1]
+            if game_name not in games_to_download:
+                continue
+            
+            print('Downloading ' + game_name)
 
 if __name__ == "__main__":
     main()
